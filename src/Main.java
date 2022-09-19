a7f836ffb60279e2448c4f89eeb24c57f7b58231
JustynaBolek <justyna.bolek@gmail.com>
2022-09-19 22:29:50 +0200

    added a new Main class

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadz wartość x");
+                int x = scanner.nextInt();
+                if (x<3){
+                    System.out.println("x jest mniejsze od 3");
+                }else {
+                    System.out.println("x jest wieksze od 3");
+                }
+    }
+}
